75374ac6e1a0eb5614b6fc87ae467650a0f325c8
tadashi <steven.t@outlook.dk>
2024-07-09 16:00:42 +0200

    modifications for installation in ubuntu noble

--- install.sh
+++ install.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 # Download the latest Neovim binary
-curl -kLO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+#curl -kLO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 
 # Remove the existing Neovim installation in /opt/nvim (if any)
-sudo rm -rf /opt/nvim
+#sudo rm -rf /opt/nvim
 
 # Extract the downloaded archive to /opt
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
+#sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
 # Add Neovim binary directory to PATH in ~/.bashrc
-echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
+#echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
 
 # Source ~/.bashrc to apply the changes
-source ~/.bashrc
+#source ~/.bashrc
 
 # Clean up the downloaded archive
-rm nvim-linux64.tar.gz
+#rm nvim-linux64.tar.gz
 
-echo "Neovim has been installed."
+#echo "Neovim has been installed."
+
+rm -rf ~/.local/share/nvim
 
 # Install NvChad
 echo -e "${GREEN}-==Installing NvChad ...==-${NC}"
@@ -29,3 +31,10 @@ echo "NvChad has been installed"
 # Replace the configs and plugins directory in ~/config/nvim/lua with the ones in the current directory
 echo -e "${GREEN}-==Copying Files==-${NC}"
 rsync -av --exclude='README.md' --exclude='.git/' --exclude='install.sh' ./ ~/.config/nvim/lua/
+
+# Open Neovim
+nvim --headless +MasonInstallAll +qa
+
+
+
+
